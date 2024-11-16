4d80e8344978cc6583aabd0fae28452e311109a6
EC2 Default User <ec2-user@ip-172-31-43-29.eu-north-1.compute.internal>
2024-11-16 12:24:18 +0000

    1116 21時

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def new
-    @book =Book.new
+    
   end
   
   def create
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
 
   def index
     @books =Book.all
+    @book =Book.new
   end
 
   def show
@@ -26,6 +27,12 @@ class BooksController < ApplicationController
     book=Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redorect_to'/books'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/new'
-  post 'books'=>'books#create'
-  get 'books' =>'books#index'
+  resources :books
   get '/top' => 'homes#top'
-    # .../books/1 や .../books/3 に該当する
-  get 'books/:id' => 'books#show' ,as:'book'
-  get 'books/:id/edit' => 'books#edit',as:'edit_book'
-  patch 'books/id' => 'books#update',as: 'update_book'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
