8f7b7ddd1ad6edaa215f51460597c530ef76559c
EC2 Default User <ec2-user@ip-172-31-43-29.eu-north-1.compute.internal>
2024-11-17 11:33:41 +0000

    完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,8 @@ class BooksController < ApplicationController
       flash[:notice]="Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      flash[:notice]="・Title can't be blank<br>
-       ・Body can't be blank"
-       render :new
+       @books =Book.all
+       render :index
     end
   end
 
@@ -25,13 +24,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+
     @book =Book.find(params[:id])
   end
-
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+       @books =Book.all
+       render :edit
+  
+    end
   end
 
   def destroy
